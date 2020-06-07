910b3ddef15273bc6c950abd88f4b461e83cf2b1
Oladimeji Vincent <60340075+vincey101@users.noreply.github.com>
2020-06-07 17:11:44 +0100

    adding files

---
+++ precache-manifest.cb7e7e43176f9b22836d414ce02ece14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9c95944407d606be19eb5ba203ec31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "bfb9b51b8ec7ad1cf0a7",
+    "url": "/static/css/main.966b963d.chunk.css"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb9b51b8ec7ad1cf0a7",
+    "url": "/static/js/main.696decfb.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
